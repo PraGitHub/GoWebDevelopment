dd506a626e5d7ce589c31253b30778b84f289786
PraGitHub <39001752+PraGitHub@users.noreply.github.com>
2019-09-07 16:25:12 +0530

    adding book to sqlite db

--- WebApp/main.go
+++ WebApp/main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
+	"encoding/xml"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"net/url"
 	"text/template"
 
 	_ "github.com/mattn/go-sqlite3"
@@ -17,10 +21,25 @@ type Page struct {
 }
 
 type SearchResult struct {
-	Title  string
-	Author string
-	Year   string
-	ID     string
+	Title  string `xml:"title,attr"`
+	Author string `xml:"author,attr"`
+	Year   string `xml:"hyr,attr"`
+	ID     string `xml:"owi,attr"`
+}
+
+type ClassifySearchResponse struct {
+	Results []SearchResult `xml:"works>work"`
+}
+
+type ClassifyBookResponse struct {
+	BookData struct {
+		Title  string `xml:"title,attr"`
+		Author string `xml:"author,attr"`
+		ID     string `xml:"owi,attr"`
+	} `xml:"work"`
+	Classification struct {
+		MostPopular string `xml:"sfa,attr"`
+	} `xml:"recommandations>ddc>mostPopular"`
 }
 
 func main() {
@@ -28,7 +47,7 @@ func main() {
 
 	templates := template.Must(template.ParseFiles("templates/index.html"))
 
-	//db, _ := sql.Open("sqlite3", "dev.db")
+	db, _ := sql.Open("sqlite3", "dev.db")
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		err := templates.ExecuteTemplate(w, "index.html", nil)
@@ -38,18 +57,113 @@ func main() {
 	})
 
 	http.HandleFunc("/search", func(w http.ResponseWriter, r *http.Request) {
-		results := []SearchResult{
-			SearchResult{"Nagarahvu", "T R Subbarao", "1972", "51JeF5abJuXT1zoNjlJe"},
-			SearchResult{"And then ther were none", "Agatha Christie", "1939", "eeSqQGbnJxMgQSk9oCDL"},
-			SearchResult{"IT", "Stephen King", "1986", "ZOlu3Nvwxz6vlTjDwLtE"},
-			SearchResult{"Eradu Kanasu", "Vani", "1974", "9YVvLgV8dN60DMg0HTGn"},
+		qs := r.FormValue("queryString")
+		log.Println("/search => qs = ", qs)
+		results, err := search(qs)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 		encoder := json.NewEncoder(w)
-		err := encoder.Encode(results)
+		err = encoder.Encode(results)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+	})
+
+	http.HandleFunc("/books/add", func(w http.ResponseWriter, r *http.Request) {
+		qs := r.FormValue("id")
+		log.Println("/book/add => qs = ", qs)
+
+		book, err := find(qs)
+		if err != nil {
+			log.Println("/books/add qs = ", qs, " error while finding ", " error = ", err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		err = db.Ping()
+		if err != nil {
+			log.Println("/books/add qs = ", qs, " DB not connected")
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		_, err = db.Exec("insert into books (pk, title, author, id, classification) values (?, ?, ?, ?, ?)",
+			nil, book.BookData.Title, book.BookData.Author, book.BookData.ID, book.Classification.MostPopular)
+		if err != nil {
+			log.Println("/books/add qs = ", qs, " error while inserting into DB error = ", err.Error())
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+
+		log.Println("/books/add qs = ", qs, " successfully inserted into db")
+
+		encoder := json.NewEncoder(w)
+		err = encoder.Encode(book)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
 
 	log.Println(http.ListenAndServe(port, nil))
+}
+
+func search(query string) (results []SearchResult, err error) {
+	var searchURL = "http://classify.oclc.org/classify2/Classify?&summary=true&title="
+	var body []byte
+	var csr ClassifySearchResponse
+
+	searchURL = searchURL + url.QueryEscape(query)
+
+	body, err = classifyAPI(searchURL)
+	if err != nil {
+		log.Println("func search :: err while requesting ", "url = ", searchURL, " error = ", err.Error())
+		return
+	}
+
+	err = xml.Unmarshal(body, &csr)
+	if err != nil {
+		log.Println("func search :: err while Unmarshalling ", "url = ", searchURL, " error = ", err.Error())
+		return
+	}
+	results = csr.Results
+
+	return
+}
+
+func find(id string) (cbr ClassifyBookResponse, err error) {
+	var searchURL = "http://classify.oclc.org/classify2/Classify?&summary=true&owi="
+	var body []byte
+
+	searchURL = searchURL + url.QueryEscape(id)
+
+	body, err = classifyAPI(searchURL)
+	if err != nil {
+		log.Println("func find :: err while requesting ", "url = ", searchURL, " error = ", err.Error())
+		return
+	}
+
+	err = xml.Unmarshal(body, &cbr)
+	if err != nil {
+		log.Println("func find :: err while Unmarshalling ", "url = ", searchURL, " error = ", err.Error())
+		return
+	}
+
+	return
+}
+
+func classifyAPI(url string) (body []byte, err error) {
+	var resp *http.Response
+
+	resp, err = http.Get(url)
+	if err != nil {
+		log.Println("func classifyAPI :: err while requesting ", "url = ", url, " error = ", err.Error())
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("func classifyAPI :: err while parsing the body ", "url = ", url, " error = ", err.Error())
+		return
+	}
+
+	return
 }
